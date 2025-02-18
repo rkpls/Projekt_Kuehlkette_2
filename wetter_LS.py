8600e6b08bbd1c067fc36411306319eb93ea1578
LarsSpychala <l.spychala@gmx.net>
2025-02-18 13:06:04 +0100

    V0.0 Test aus Skript

---
+++ wetter_LS.py
@@ -0,0 +1,20 @@
+import requests
+import json
+from datetime import datetime
+# Beispiel-Nutzung
+api_key = "EFVT5R3796ZG2QMKN7KCRLYF4"
+location = "26127,DE"
+datetime_str = "18.02.2025 13:00" # Zeit auf die nächste volle Stunde gerundet
+# Konvertiere das Datum und die Uhrzeit in das erforderliche Format
+datetime_obj = datetime.strptime(datetime_str, '%d.%m.%Y %H:%M')
+timestamp = datetime_obj.strftime('%Y-%m-%dT%H:%M:%S')
+# Visual Crossing Weather API-Endpunkt
+url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{timestamp}'.format(location=location, timestamp=timestamp)
+response = requests.get(url, params={'unitGroup': 'metric','key':
+api_key,'include': 'hours'})
+data = response.json()
+# Ausgabe der Temperatur
+print("\nTemperatur: ", data["days"][0]["temp"],"\n")
+# Ausgabe des gesamten JSON-Objekts
+#json_str = json.dumps(data, indent=4)
+#print(json_str)
